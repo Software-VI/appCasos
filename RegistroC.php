ac7674b3a99ba6fd457190b30b0bc33c6ecec67e
CesarJavier09 <CesarIglesias@UTP-github.com>
2021-07-11 11:26:05 -0500

    Simplificado Registro C
    Código mas sencillo

--- RegistroC.php
+++ RegistroC.php
@@ -17,34 +17,18 @@
         $resultado_insert = mysqli_query($conexion, $insert);
 
         if ($resultado_insert){
-            $consulta = "SELECT * FROM ciudadanos WHERE cedula = '$cedula'";
-            $resultado = mysqli_query($conexion, $consulta);
-
-            if ($registro = mysqli_fetch_array($resultado)){
-                $json['usuario']=$registro;
-            }
+           
+            $json['response']="Success";
             mysqli_close($conexion);
             echo json_encode($json);
         }
         else{
-            $resulta["cedula"] = 0;
-            $resulta["nombre"] = 'no registra';
-            $resulta["apellido"] = 'no registra';
-			$resulta["direccion"] = 'no registra';
-            $json['usuario'][] = $resulta;
+			$json['response'] = "Error";
             echo json_encode($json);
         }
     }
     else{
-        $resulta["cod_unidad"] = 0;
-        $resulta["usuario"] = 'Web Service no retorna';
-        $resulta["nombre"] = 'Web Service no retorna';
-        $resulta["apellido"] = 'Web Service no retorna';
-        $resulta["password"] = 'Web Service no retorna';
-        $resulta["email"] = 'Web Service no retorna';
-        $resulta["telefono"] = 'Web Service no retorna';
-        $resulta["rango"] = 'Web Service no retorna';
-        $json['usuario'][] = $resulta;
+        $json['response'] = "Error";
         echo json_encode($json);
     }
 ?>
